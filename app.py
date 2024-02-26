8d8e30e5b2fe5b0b643e4cf162edcfe65b571dc0
Noah Torres <43016877+noah51022@users.noreply.github.com>
2024-02-26 18:07:17 -0500

    Add files via upload
    This file contains the updated code that allows users to upload multiple CSV files

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from pandasai.llm.openai import OpenAI
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from pandasai import PandasAI
+from pandasai import SmartDataframe
 
 load_dotenv()
 
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("API_KEY")
 
 
-def chat_with_csv(df,prompt):
-    llm = OpenAI(api_token=openai_api_key)
-    pandas_ai = PandasAI(llm)
-    result = pandas_ai.run(df, prompt=prompt)
+def chat_with_csv(df, prompt):
+    llm = OpenAI(api_token="API_KEY")
+    df = SmartDataframe(df, config={"llm": llm})
+    result = df.chat(prompt)
     print(result)
     return result
 
@@ -22,15 +22,18 @@ st.set_page_config(layout='wide')
 
 st.title("ChatCSV powered by LLM")
 
-input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
+input_csv = st.file_uploader("Upload your CSV file", type=['csv'], accept_multiple_files=True)
 
-if input_csv is not None:
+if input_csv:
+        
+        selected_file = st.selectbox("Select a CSV file", [file.name for file in input_csv])
+        selected_index = [file.name for file in input_csv].index(selected_file)
 
         col1, col2 = st.columns([1,1])
 
         with col1:
             st.info("CSV Uploaded Successfully")
-            data = pd.read_csv(input_csv)
+            data = pd.read_csv(input_csv[selected_index])
             st.dataframe(data, use_container_width=True)
 
         with col2:
@@ -39,7 +42,7 @@ if input_csv is not None:
             
             input_text = st.text_area("Enter your query")
 
-            if input_text is not None:
+            if input_text:
                 if st.button("Chat with CSV"):
                     st.info("Your Query: "+input_text)
                     result = chat_with_csv(data, input_text)
